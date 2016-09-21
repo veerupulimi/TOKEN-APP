379824bd4dad175b34af21f655a97b1eb1671031
veerupulimi86 <veerupulimi86@gmail.com>
2016-09-21 18:56:32 +0530

    first commit

---
+++ src/main/java/com/lts/app/LTS_APP/App.java
@@ -0,0 +1,13 @@
+package com.lts.app.LTS_APP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
